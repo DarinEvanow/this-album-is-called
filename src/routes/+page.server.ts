d12bd1c69962e7523d3c6c58662d9aa5be12878b
Darin Evanow <darinevanow@gmail.com>
2024-03-27 16:47:17 -0700

    Comment out API call so I don't run up my bill.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,12 +4,12 @@ import { OPENAI_API_KEY } from '$env/static/private';
 export const load: any = async () => {
   const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
-  const response = await openai.images.generate({
-    model: "dall-e-3",
-    prompt: "a white siamese cat",
-    n: 1,
-    size: "1024x1024",
-  });
+  // const response = await openai.images.generate({
+  //   model: "dall-e-3",
+  //   prompt: "a white siamese cat",
+  //   n: 1,
+  //   size: "1024x1024",
+  // });
 
   const image_url = response.data[0].url;
 
